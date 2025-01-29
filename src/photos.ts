1bbe75a7f428f58f8ebc3ec698596bc3284c3c22
crnobog <carskalavra@proton.me>
2025-01-29 21:58:53 +0100

    ❄️

---
+++ src/photos.ts
@@ -0,0 +1,49 @@
+import type { Photo } from "../types";
+
+export const photos: Photo[] = [
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/eto1.jpg?t=2024-12-31T20%3A41%3A18.470Z",
+    alt: "eto1.jpg",
+    title: "Јошимура Ето",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/rize1.jpg",
+    alt: "rize1.jpg",
+    title: "Камиширо Ризе",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/rize2.jpg",
+    alt: "rize2.jpg",
+    title: "Камиширо Ризе",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/rize3.jpg",
+    alt: "rize3.jpg",
+    title: "Камиширо Ризе",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/hinami1.jpg",
+    alt: "hinami1.jpg",
+    title: "Фуегучи Хинами",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/rize4.jpg",
+    alt: "rize4.jpg",
+    title: "Камиширо Ризе",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/kuroshiro.jpg",
+    alt: "kuroshiro.jpg",
+    title: "Куро и Широ",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/touka1.jpg",
+    alt: "touka1.jpg",
+    title: "Киришима Тока",
+  },
+  {
+    src: "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/photos/eto2.jpg",
+    alt: "eto2.jpg",
+    title: "Јошимура Ето",
+  },
+];
---
+++ src/tracks.ts
@@ -0,0 +1,120 @@
+export interface Track {
+  id: string;
+  title: string;
+  artist: string;
+  album: string;
+  cover: string;
+  audioSrc: string;
+  sourceUrl: string;
+}
+
+export const tracks: Track[] = [
+  {
+    id: "1",
+    title: "Missing Textures - Slowed Down",
+    artist: "NIVEK FFORHS",
+    album: "Missing Textures",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/missing-textures.webp?t=2024-12-31T19%3A52%3A50.382Z",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/missing-textures.mp3?t=2024-12-31T20%3A09%3A41.103Z",
+    sourceUrl:
+      "https://open.spotify.com/track/5u7Ow7JQjTFnrQoA0myBqI?si=9590bcde1a274beb",
+  },
+  {
+    id: "2",
+    title: "Mis",
+    artist: "Alex G",
+    album: "Rules",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/mis.webp",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/mis.mp3?t=2024-12-31T20%3A09%3A48.540Z",
+    sourceUrl:
+      "https://open.spotify.com/track/0lGTVZfU82bYT2uJw22Kz4?si=5ab9a9c8f877434b",
+  },
+  {
+    id: "3",
+    title: "Keep It Tucked",
+    artist: "ThxSoMuch",
+    album: "Keep It Tucked",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/keepittucked.png",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/keepittucked.mp3?t=2024-12-31T20%3A10%3A00.570Z",
+    sourceUrl:
+      "https://open.spotify.com/track/1EdQCb51lC8usq47IMhADP?si=79843b87a8d34971",
+  },
+  {
+    id: "4",
+    title: "Monolith",
+    artist: "Twin Tribes",
+    album: "Monolith",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/monolith.webp",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/monolith.mp3?t=2024-12-31T20%3A10%3A07.416Z",
+    sourceUrl:
+      "https://open.spotify.com/track/3U0UXxBIfjUsJ8RtxoxUFn?si=c87650e16a354804",
+  },
+  {
+    id: "5",
+    title: "loud room",
+    artist: "lumisoun",
+    album: "loud room",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/loudroom.jpg",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/loudroom.mp3?t=2024-12-31T20%3A10%3A18.630Z",
+    sourceUrl:
+      "https://open.spotify.com/track/2bDI4MoDC6eiaXrJExSmbB?si=c67d7e4d38424848",
+  },
+  {
+    id: "6",
+    title: "Kuchuu Buranko",
+    artist: "ROCKSTXR!, Ronerom",
+    album: "Kuchuu Buranko",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/KuchuuBuranko.jpeg",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/KuuchuBuranko.mp3?t=2024-12-31T20%3A10%3A24.339Z",
+    sourceUrl:
+      "https://open.spotify.com/track/76wYJ52u3p4eRzMdHomDag?si=6610c5303c34400a",
+  },
+  {
+    id: "7",
+    title: "STYX HELIX",
+    artist: "MYTH & ROID",
+    album: "eYe's",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/styxhelix.jpeg",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/styxhelix.mp3?t=2024-12-31T20%3A10%3A30.773Z",
+    sourceUrl:
+      "https://open.spotify.com/track/2tcSz3bcJqriPg9vetvJLs?si=f2df9e4afc2b47ee",
+  },
+  {
+    id: "8",
+    title: "welcome and goodbye",
+    artist: "Dream, Ivory",
+    album: "welcome and goodbye",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/welcomeandgoodbye.jpeg",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/welcomeandgoodbye.mp3?t=2024-12-31T20%3A10%3A38.103Z",
+    sourceUrl:
+      "https://open.spotify.com/track/00RLNHc2jkEjUoCUlFgPVT?si=61d18b62ab234d54",
+  },
+  {
+    id: "9",
+    title: "Memory Reboot - Sped Up",
+    artist: "VØJ, Narvent",
+    album: "Memory Reboot- Sped Up",
+    cover:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/artwork/memoryreboot.jpeg",
+    audioSrc:
+      "https://vzbsygkonwqvfrqkphuh.supabase.co/storage/v1/object/public/cgalerija/audio/song/memoryreboot.mp3?t=2024-12-31T20%3A10%3A45.620Z",
+    sourceUrl:
+      "https://open.spotify.com/album/46Mt99EZGmVa4GFvfQyZLE?si=oBtsYsSxSR69TOqav7jMkQ",
+  },
+];
